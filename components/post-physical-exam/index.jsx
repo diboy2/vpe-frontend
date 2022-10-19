eec55902b5230d8d76ebe37e1c9e4f75c664937d
Adrian Cagaanan <acagaana@villanova.edu>
2022-10-19 09:45:31 -0400

    Save stuff

--- components/post-physical-exam/index.jsx
+++ components/post-physical-exam/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import Grid from "@mui/material/Grid"
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LinearProgress from "@mui/material/LinearProgress"
 
-const PostPhysicalExamination = () => {
+const PostPhysicalExamination = () => { 
 
     return (
         <Grid container display="flex" height="100%">
+            <Box sx={{ width: '100%', height:'100%' }}>
+                <LinearProgress />
+            </Box>
         </Grid>
     )
 };
--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,11 @@ import '../styles/globals.css'
 import Grid from '@mui/material/Grid';
 function MyApp({ Component, pageProps }) {
   return (
-    <Grid container display="flex" justifyContent="center" height="100vh">
-      <Grid item xs={6} md={8}>
+    // <Grid container display="flex" justifyContent="center" height="100vh">
+    //   <Grid item xs={6} md={8}>
         <Component {...pageProps} />
-      </Grid>
-    </Grid>
+    //   </Grid>
+    // </Grid>
   );
 }
 
