737035629a4e4057ef9e9159e866a266990021f8
Adrian Cagaanan <acagaana@villanova.edu>
2022-10-19 09:19:09 -0400

    Save stuff

--- components/physical-exam/index.jsx
+++ components/physical-exam/index.jsx
@@ -31,29 +31,16 @@ const steps = [
     label: 'Heart'
   },
   {
-    label: 'Abdomen',
-    description: `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`,
+    label: 'Abdomen'
   },
   {
-    label: 'Extremities',
-    description:
-      'An ad group contains one or more ads which target a shared set of keywords.',
+    label: 'Extremities'
   },
   {
-    label: 'Neurological',
-    description: `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`,
+    label: 'Neurological'
   },
   {
-    label: 'Social Determinants of Health',
-    description: `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`,
+    label: 'Social Determinants of Health'
   },
 ];
 
